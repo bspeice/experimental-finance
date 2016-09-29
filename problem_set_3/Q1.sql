e39a1b5cb2e4464bddce5c0d9f7eaec59db15a52
gg2607 <gg2607@columbia.edu>
2016-09-29 19:56:57 -0400

    Fingers crossed

--- problem_set_3/Q1.sql
+++ problem_set_3/Q1.sql
@@ -1,47 +1,35 @@
 -- CSCO 103042
-DECLARE @SecurityID INT = 103042
+declare @SecurityID int = 103042
 
 -- Get all relevant data
-SELECT
-  op.Date,
-  sp.ClosePrice                         AS StockPrice,
-  op.CallPut,
-  op.Expiration,
-  XF.dbo.formatStrike(op.Strike)        AS Strike,
-  XF.dbo.mbbo(op.BestBid, op.BestOffer) AS MBBO,
-  op.ImpliedVolatility
-INTO #data
-FROM XFDATA.dbo.OPTION_PRICE_VIEW op
-  INNER JOIN XFDATA.dbo.SECURITY_PRICE sp ON sp.SecurityID = op.SecurityID AND sp.Date = op.Date
-WHERE op.SecurityID = @SecurityID
-      AND op.Date BETWEEN '2007-1-1' AND '2007-12-31'
-      AND op.CallPut = 'C'
+select op.Date, sp.ClosePrice as StockPrice, op.CallPut, op.Expiration, XF.dbo.formatStrike(op.Strike) as Strike,
+  XF.dbo.mbbo(op.BestBid,op.BestOffer) as MBBO, op.ImpliedVolatility
+into #data
+from XFDATA.dbo.OPTION_PRICE_VIEW op
+  inner join XFDATA.dbo.SECURITY_PRICE sp on sp.SecurityID = op.SecurityID and sp.Date = op.Date
+where op.SecurityID = @SecurityID
+  and op.Date between '2007-1-1' and '2007-12-31'
+  and op.CallPut = 'C'
 
 -- Extract the 2nd series
-SELECT
-  *,
-  ROUND(ABS((SIGN(d1.StockPrice - d1.Strike) + 1) / 2 * (d1.StockPrice - d1.Strike)), 2) AS IntrinsicValue
-INTO #SecondSeries
-FROM #data d1
-WHERE Expiration = (
-  SELECT min(Expiration)
-  FROM #data d2
-  WHERE d2.Date = d1.Date
-        AND Expiration > (SELECT min(Expiration)
-                          FROM #data d3
-                          WHERE d3.Date = d1.Date
-                          GROUP BY Date)
-  GROUP BY Date
+select *,  ROUND(ABS((SIGN(d1.StockPrice-d1.Strike) + 1 ) / 2 * (d1.StockPrice-d1.Strike)),2) as IntrinsicValue
+into #SecondSeries
+from #data d1
+where Expiration = (
+  select min(Expiration)
+  from #data d2
+  where d2.Date = d1.Date
+    and Expiration > (select min(Expiration) from #data d3 where d3.Date = d1.Date group by Date)
+  group by Date
 )
 
 -- Display second series options with POP > 0.5 
-SELECT
-  *,
-  MBBO - IntrinsicValue AS POP,
-  Strike / StockPrice   AS Moneyness
-FROM #SecondSeries
-WHERE ImpliedVolatility > 0
-      AND MBBO - IntrinsicValue > 0.5
-ORDER BY Date, Strike / StockPrice ASC
+select *,
+  MBBO - IntrinsicValue as POP,
+  Strike/StockPrice as Moneyness
+from #SecondSeries
+where ImpliedVolatility > 0
+  and MBBO - IntrinsicValue > 0.5
+order by Date, Strike/StockPrice asc
 
-DROP TABLE #data, #SecondSeries
+drop table #data, #SecondSeries

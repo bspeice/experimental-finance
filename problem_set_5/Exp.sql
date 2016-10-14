969e936f39439f92ed6a52e7ae7f3795b295491b
ggambarotta <g.gambarotta@gmail.com>
2016-10-14 18:04:06 -0400

    Q2 fix

--- problem_set_5/Exp.sql
+++ problem_set_5/Exp.sql
@@ -10,13 +10,24 @@ from XFDATA.dbo.OPTION_PRICE_VIEW
 where SecurityID = @SecurityID and Date between @DateStart and @DateEnd
 
 update e set
-  [First] = (select min(Expiration) from #Expirations where Date=cast(e.EarningDate as date) and Expiration > cast(e.EarningDate as date)),
-  [Second] = (select min(Expiration) from #Expirations where Date=cast(e.EarningDate as date) and Expiration > cast(e.EarningDate as date)
-                                                             and Expiration > (select min(Expiration)
-                        from #Expirations where Date=cast(e.EarningDate as date) and Expiration > cast(e.EarningDate as date))),
+  [First] = (select min(Expiration) from #Expirations where Date=cast(e.EarningDate as date) 
+					and (Expiration-cast(e.EarningDate as date))>(CASE WHEN DATEPART(WEEKDAY,Expiration)=8 THEN 1 ELSE 0 END)
+					and datepart(day, Expiration)>=15 
+					and datepart(day, Expiration)<=(CASE WHEN DATEPART(WEEKDAY,Expiration)=7 THEN 22 ELSE 21 END)),
+
+  [Second] = (select min(Expiration) from #Expirations where Date=cast(e.EarningDate as date) 
+					and (Expiration-cast(e.EarningDate as date))>(CASE WHEN DATEPART(WEEKDAY,Expiration)=8 THEN 1 ELSE 0 END)
+                             	        and datepart(day, Expiration)>=15 
+					and datepart(day, Expiration)<=(CASE WHEN DATEPART(WEEKDAY,Expiration)=7 THEN 22 ELSE 21 END) 
+					and Expiration > (select min(Expiration)
+                       		        from #Expirations where Date=cast(e.EarningDate as date) 
+					and (Expiration-cast(e.EarningDate as date))>(CASE WHEN DATEPART(WEEKDAY,Expiration)=8 THEN 1 ELSE 0 END)
+					and datepart(day, Expiration)>=15 
+					and datepart(day, Expiration)<=(CASE WHEN DATEPART(WEEKDAY,Expiration)=7 THEN 22 ELSE 21 END))),
+
   [JanLeap] = (select min(Expiration) from #Expirations where Date=cast(e.EarningDate as date)
-                and Expiration > cast(e.EarningDate as date)
-                and datediff(month,e.EarningDate,Expiration) > 5 and month(Expiration) = 1)
+               				and Expiration > cast(e.EarningDate as date)
+               			        and datediff(month,e.EarningDate,Expiration) > 5 and month(Expiration) = 1)
 from Earnings e
 
 select * from Earnings

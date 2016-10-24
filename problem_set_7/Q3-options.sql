36b74866fd26258098670348c5aa3e58cfd6307d
bspeice <bspeice.nc@gmail.com>
2016-10-24 19:00:11 -0400

    Add in the zero-curve rates Currently picks the rate (on the trading day) with maturity closest to expiration

--- problem_set_7/Q3-options.sql
+++ problem_set_7/Q3-options.sql
@@ -7,12 +7,15 @@ SELECT
   XF.dbo.formatStrike(op.Strike)        AS AdjStrike,
   op.Expiration,
   sp.ClosePrice,
-  DATEDIFF(DAY, sp.Date, op.Expiration) AS TTE
+  zc.Rate,
+  ABS(DATEDIFF(DAY, sp.Date, op.Expiration)) AS TTE_sec,
+  ABS(DATEDIFF(DAY, zc.Date + zc.Days, op.Expiration)) AS TTE_zc
 INTO #diff_table
 FROM XFDATA.dbo.OPTION_PRICE_VIEW op
   INNER JOIN XFDATA.dbo.SECURITY_NAME sn ON op.SecurityID = sn.SecurityID
   INNER JOIN XFDATA.dbo.SECURITY_PRICE sp ON op.SecurityID = sp.SecurityID
                                              AND op.Date = sp.Date
+  INNER JOIN XFDATA.dbo.ZERO_CURVE zc ON op.Date = zc.Date
 
 WHERE sn.Ticker = 'CEPH'
       AND op.Date BETWEEN '2011-01-01' AND '2011-06-01'
@@ -27,16 +30,20 @@ SELECT
   #diff_table.BestBid,
   #diff_table.BestOffer,
   #diff_table.Expiration,
-  #diff_table.TTE,
-  #diff_table.CallPut
+  #diff_table.TTE_sec,
+  #diff_table.CallPut,
+  #diff_table.TTE_zc,
+  #diff_table.Rate AS ZC_Rate
 FROM #diff_table
   INNER JOIN (
                SELECT
-                 MIN(#diff_table.TTE) AS MinTTE,
+                 MIN(#diff_table.TTE_sec) AS MinTTE_sec,
+                 MIN(#diff_table.TTE_zc)  AS MinTTE_zc,
                  #diff_table.Date
                FROM #diff_table
                GROUP BY #diff_table.Date
-             ) dt_grouped ON #diff_table.TTE = dt_grouped.MinTTE
+             ) dt_grouped ON #diff_table.TTE_sec = dt_grouped.MinTTE_sec
+                             AND #diff_table.TTE_zc = dt_grouped.MinTTE_zc
 
 GROUP BY
   #diff_table.Date,
@@ -46,8 +53,10 @@ GROUP BY
   #diff_table.BestBid,
   #diff_table.BestOffer,
   #diff_table.Expiration,
-  #diff_table.TTE,
-  #diff_table.CallPut
+  #diff_table.TTE_sec,
+  #diff_table.CallPut,
+  #diff_table.TTE_zc,
+  #diff_table.Rate
 
 ORDER BY
   #diff_table.Date,
